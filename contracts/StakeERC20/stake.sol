89f4fae0a0844d88b9ebda628e3841092771f13d
koxy <devkoxy@gmail.com>
2024-08-30 11:51:40 +0100

    project set up

---
+++ contracts/StakeERC20/stake.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+//ERC20 Staking Smart Contract
+
+//Objective: Write an ERC20 staking smart contract that allows users to stake a specific ERC20 token for rewards.
+
+//Requirements:
+//Users should be able to stake the ERC20 token by transferring the tokens to the contract.
+//The contract should track the amount and duration of each user’s stake.
+//Implement a reward mechanism similar to the Ether staking contract, where rewards are based on the staking duration.
+//Users should be able to withdraw their staked tokens and the rewards after the staking period.
+//The contract should handle ERC20 token transfers securely and efficiently.
+
+
+contract StakeERC20 {}
---
+++ contracts/StakeEther/stake.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+//Write an Ether staking smart contract that allows users to stake Ether for a specified period.
+
+//Requirements:
+//Users should be able to stake Ether by sending a transaction to the contract.
+//The contract should record the staking time for each user.
+//Implement a reward mechanism where users earn rewards based on how long they have staked their Ether.
+//The rewards should be proportional to the duration of the stake.
+//Users should be able to withdraw both their staked Ether and the earned rewards after the staking period ends.
+//Ensure the contract is secure, especially in handling users’ funds and calculating rewards.
+
+
+contract StakeEther {}
