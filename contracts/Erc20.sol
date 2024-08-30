1e0e4f02587ea82dabf37b781b6d619ff33f9009
koxy <devkoxy@gmail.com>
2024-08-30 11:40:16 +0100

    staking on ether and with erc20 setup

---
+++ contracts/Erc20.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract MyERC20 is ERC20 {
+
+    address public owner;
+
+    constructor() ERC20("MyERC20", "MY") {
+        owner = msg.sender;
+         _mint(msg.sender, 10000e18);
+
+    }
+
+}
